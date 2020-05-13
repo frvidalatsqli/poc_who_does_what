e32e401e257150c279dc5682567e5a29eed5e331
frvidal <frederic.vidal.perso@gmail.com>
2020-05-13 09:22:23 +0200

    First release of POC

---
+++ init.sh
@@ -0,0 +1,6 @@
+echo "# POC who does what?" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://frvidalatsqli@github.com/frvidalatsqli/poc_who_does_what.git
+git push -u origin master
